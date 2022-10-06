f586ab03e52fc04591bbac50e12caf936989ddfc
Romchik234 <romakhazov38@gmail.com>
2022-10-06 15:09:53 +0200

    adasda06.10.2022

--- Programowanie-1E/TasksWithVariables/TasksWithVariables.cpp
+++ Programowanie-1E/TasksWithVariables/TasksWithVariables.cpp
@@ -4,21 +4,21 @@ napisz funkcjê która pobierze dwie liczby oraz wykona podstawowe operacje aretme
 */
 void task1()
 {
-	int firstNumber, secondNumber; 
-	std::cout << "Podaj liczbe pierwsza:" << "\n"; 
-	std::cin >> firstNumber; 
+	int firstNumber, secondNumber;
+	std::cout << "Podaj liczbe pierwsza:" << "\n";
+	std::cin >> firstNumber;
 
-	std::cout << "Podaj druga liczbe: \n"; 
-	std::cin >> secondNumber; 
+	std::cout << "Podaj druga liczbe: \n";
+	std::cin >> secondNumber;
 
 	//  suma,   ró¿nica, iloczyn,  iloraz , reszta dzielenia; 
-	int sum, difference, product, quotient, rest; 
-	
-	sum = firstNumber + secondNumber; 
+	int sum, difference, product, quotient, rest;
+
+	sum = firstNumber + secondNumber;
 	difference = firstNumber - secondNumber;
-	product = firstNumber * secondNumber; 
-	quotient = firstNumber / secondNumber; 
-	rest = firstNumber % secondNumber; 
+	product = firstNumber * secondNumber;
+	quotient = firstNumber / secondNumber;
+	rest = firstNumber % secondNumber;
 
 	std::cout << "Suma:" << sum << "\n";
 	std::cout << "Roznica:" << difference << "\n";
@@ -27,12 +27,21 @@ void task1()
 	std::cout << "reszta_z_dzielenia:" << rest << "\n";
 }
 
+//Przetestowaæ ró¿ne warianry dzielenia
+void task2 ()
+{
+
+	std::cout << "15 / 4 =  " << (15 / 4) << "\n";
+	std::cout << "15.0 / 4 =  " << (15.0 / 4) << "\n";
+	std::cout << "15 / 4.0 =  " << (15 / 4.0) << "\n";
+	std::cout << "15.0 / 4.0 =  " << (15.0 / 4.0) << "\n";
+}
 
 int main()
 {
  
-	task1();
-
+	//task1();
+	task2();
 
 
 
