927a425b13d0d3e6b70f0a405502fb224b6472ea
Romchik234 <romakhazov38@gmail.com>
2022-10-20 15:13:17 +0200

    20.10.2022

--- Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
+++ Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 /*
+DRY - don't repeat yourself - nie powtarzaj siê 
+*/
+
+
+
+
+/*
 Napisz funkcjê która wczyta dwie liczby 
 i wykona dzielenia na nich 
 */
+
 
 
 void task1()
@@ -18,7 +26,7 @@ void task1()
 	// iloraz; 
 	int quotient;
 
-	if (secondNumber != 0)
+/*	if (secondNumber != 0)
 		quotient = firstNumber / secondNumber;
 
 	if (secondNumber != 0)
@@ -26,7 +34,15 @@ void task1()
 	
 	if (secondNumber == 0)
 		std::cout << "Dzielenie przez zero !!! \n"; 
+		tak robiæ nie wolno by my siê powtarzamy "if"			*/
 
+	if (secondNumber != 0)
+	{
+		quotient = firstNumber / secondNumber;
+		std::cout << "Iloraz:" << quotient << "\n";
+	}
+	if (secondNumber == 0)
+		std::cout << "Dzielenie przez zero !!! \n";
 }
 
 int main()
