8a607c20101a67a42a6c2ebd3faf09bb4c51a130
Romchik234 <romakhazov38@gmail.com>
2022-10-26 09:54:56 +0200

    26.10.2022

--- Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
+++ Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
@@ -42,16 +42,35 @@ void task1()
 		std::cout << "Iloraz:" << quotient << "\n";
 	}
 	else /*w przeciwnym wypadku*/
-	
 		std::cout << "Dzielenie przez zero !!! \n";
 	
 	std::cout << "Koniec programu1 \n";
 
 }
+ 
+/*
+Napidz funkcjê która wczyta liczbê i wyœwietli informacjê czy jest ona dodatnia czy nie 
+*/
+
+void task2()
+{
+	int NumberToCheck;
+	std::cout << "Podaj liczbe do sprawdzenia:" << "\n";
+	std::cin >> NumberToCheck;
+
+	if (NumberToCheck = <0)
+		std::cout << "liczba jest ujemna \n"; 
+	else
+	{
+		std::cout << "Liczba jest dodatnia";
+	}
+
+}
 
 int main()
 {
-	task1();
+	//	task1();	
+	task2();
 
 
 }
