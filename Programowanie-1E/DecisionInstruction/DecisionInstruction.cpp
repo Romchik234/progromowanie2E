371ad5063af7a059d3043befc5469a677c58f7e9
Romchik234 <romakhazov38@gmail.com>
2022-11-03 15:06:03 +0100

    03.11.2022

--- Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
+++ Programowanie-1E/DecisionInstruction/DecisionInstruction.cpp
@@ -1,37 +1,50 @@
 #include <iostream>
 
 /*
-DRY - don't repeat yourself - nie powtarzaj siê 
+DRY - don't repeat yourself - nie powtarzaj siê
 */
 
 /*
-Operatory porównania: 
+Operatory porównania:
 
-*równoœci			==	
+*równoœci			==
 *ró¿noœci			!=
 *wiêkszoœæ			>
-*mniejsze			< 
-*wieksze lub równe	>= 
+*mniejsze			<
+*wieksze lub równe	>=
 *mniejsze lub równe	<=
 
-aregument1 >= argument2 
+aregument1 >= argument2
 
-Wynikiem dzia³ania tych operatorów 
-jest wartoœæ logiczna true/false typu bool 
+Wynikiem dzia³ania tych operatorów
+jest wartoœæ logiczna true/false typu bool
 
-bool zmienna = true; 
-bool zmienna2 = 5 > 74; 
+bool zmienna = true;
+bool zmienna2 = 5 > 74;
+
+*/
+
+/*
+Operatory logiczne:
+F-false
+T-true 
+
+			and				or 
+A	B	  A && B	      A || B
+
+F	F		F				F
+F	T		F				T
+T	F		F				T
+T	T		T				T
+
 
 */
 
 
 /*
-Napisz funkcjê która wczyta dwie liczby 
-i wykona dzielenia na nich 
+Napisz funkcjê która wczyta dwie liczby
+i wykona dzielenia na nich
 */
-
-
-
 void task1()
 {
 	int firstNumber, secondNumber;
@@ -61,14 +74,13 @@ void task1()
 	}
 	else /*w przeciwnym wypadku*/
 		std::cout << "Dzielenie przez zero !!! \n";
-	
-	std::cout << "Koniec programu1 \n";	
-}
- 
-/*
-Napidz funkcjê która wczyta liczbê i wyœwietli informacjê czy jest ona dodatnia czy nie 
-*/
 
+	std::cout << "Koniec programu1 \n";
+}
+
+/*
+Napidz funkcjê która wczyta liczbê i wyœwietli informacjê czy jest ona dodatnia czy nie
+*/
 void task2()
 {
 	int NumberToCheck;
@@ -88,11 +100,10 @@ void task2()
 }
 
 /*
-Napisz funkcjê, która wczyta liczbêi wyœwietli informacjê 
+Napisz funkcjê, która wczyta liczbêi wyœwietli informacjê
 czy jest ona w zakresie <10,25>.
 
 */
-
 void task3()
 {
 	int NumberToCheck;
@@ -100,6 +111,7 @@ void task3()
 	std::cout << "Podaj liczbe do sprawdzenia (	¿eby spewnia³ warunek <10/25>):" << "\n";
 	std::cin >> NumberToCheck;
 
+	/*
 	if (NumberToCheck >= 10)
 	{
 		if (NumberToCheck <= 25)
@@ -109,13 +121,18 @@ void task3()
 	}
 	else
 		std::cout << "liczba nie spewnia warunek";
-	
+	*/
+
+	if (NumberToCheck >= 10 && NumberToCheck <= 25)
+		std::cout << "liczba jest w zakresie \n";
+	else
+		std::cout << "liczba nie jest w zakresie \n";
+
 }
 
 int main()
 {
-	//	task1();	
+	//task1();	
 	//task2();
 	task3();
-
 }
