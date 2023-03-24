a72e1646b0894757c09a969d72c8ccb010f2e052
Romchik234 <romakhazov38@gmail.com>
2023-03-24 15:13:52 +0100

    24.03.2023

--- Programowanie-1E/PeselConcoleApplication/PeselConcoleApplication.cpp
+++ Programowanie-1E/PeselConcoleApplication/PeselConcoleApplication.cpp
@@ -13,17 +13,30 @@ std::string getpesel()
 	return stringPesel;
 }
 
-bool cheackPesel(std::string stringPesel)
+bool cheackPeselLenght(std::string stringPesel, std::string& errorMassage)
 {
 	//dlugosc znakow (11)
 	if (stringPesel.length() != 11)
+	{
+		errorMassage = "Pelsel musi miec 11 znakow!";
+		return false;
+	}
+
+	return true; 
+}
+
+bool cheackPesel(std::string stringPesel, std::string& errorMassage)
+{
+	if (cheackPeselLenght(stringPesel, errorMassage) == false)
 		return false; 
+
 	//tylko cyfry 
 	for (int i = 0; i < 11; i++)
 	{
 		if (stringPesel[i] < '0'
 			|| stringPesel[i] > '9')
 		{
+			errorMassage = "Pesel musi zawierac tylko cyfry"; 
 			return false ; 
 		}
 	}
@@ -36,14 +49,15 @@ bool cheackPesel(std::string stringPesel)
 int main()
 {
 	 std::string stringPesel = getpesel(); 
+	 std::string errorMassage;
 
-	 if (cheackPesel(stringPesel) == true)
+	 if (cheackPesel(stringPesel, errorMassage) == true)
 	 {
 		 std::cout << "Pesel jest poprawny\n";
 	 }
 	 else
 	 {
-
+		 std::cout << errorMassage << "\n";
 	 }
 	//walidacja poprawnosci numeru pesel 
 
