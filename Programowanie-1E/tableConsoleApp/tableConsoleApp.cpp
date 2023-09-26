80f666bfa7df7d5bc049ca25c3d3a46146d0b61d
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-26 15:13:48 +0200

    123123213

--- Programowanie-1E/tableConsoleApp/tableConsoleApp.cpp
+++ Programowanie-1E/tableConsoleApp/tableConsoleApp.cpp
@@ -18,22 +18,46 @@ void task2()
 	int smaller = 9999;
 
 	srand(time(NULL));
-	for (int i = 0;  i < size;  i++)
+	for (int i = 0; i < size; i++)
 	{
-		tabOfNumbers[i] = rand() %  1000;
+		tabOfNumbers[i] = rand() % 1000;
 		if (tabOfNumbers[i] < smaller)
 			smaller = tabOfNumbers[i];
 	}
 	std::cout << "najmniejsza liczba to:" << smaller << "\n";
-	std::cout << "Liczby w tablice : \n"; 
+	std::cout << "Liczby w tablice : \n";
 	for (int i = 0; i < size; i++)
 	{
-		std::cout << tabOfNumbers[i] << "\n"; 
+		std::cout << tabOfNumbers[i] << "\n";
 	}
+}
+
+void zadanieoue()
+{
+	const int tableSize = 3;
+	int tab[tableSize]; 
+	float avarageOfNumber; 
+	float sumeOfNumbers = 0;
+
+	srand(time(NULL));
+	for (int i = 0; i < tableSize; i++)
+	{
+		tab[i] = rand() % 10; 
+		sumeOfNumbers = sumeOfNumbers + tab[i]; 
 	}
+ 
+	avarageOfNumber = sumeOfNumbers / tableSize; 
+	
+	for (int i = 0; i  < tableSize; i ++)
+	{
+		std::cout << tab[i] << ", ";
+	}
+	std::cout << "\n" << "srednia = " << avarageOfNumber; 
+}
 
 int main()
 {
 	//task1();
-	task2();
+	//task2();
+	zadanieoue();
 }
