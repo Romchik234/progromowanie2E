0406b0ca19dca485d8011e2ae3f7a699076f4288
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-14 15:10:26 +0100

    14.12.2023

--- Programowanie-1E/LibraryConsolAplication/LibraryConsolAplication.cpp
+++ Programowanie-1E/LibraryConsolAplication/LibraryConsolAplication.cpp
@@ -1,20 +1,56 @@
-// LibraryConsolAplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+struct book
+{
+    std::string title;
+    std::string author;
+    short publicationYear;
+    unsigned short numerOfPages;
+};
+
+void menu()
+{
+    system("cls");
+    std::cout << "BIBLIOTEKA\n";
+    std::cout << "1.Dodanoe nowej ksia¿ki \n";
+
+    std::cout << "0.Koniec programu\n";
+
+}
+
+int getOption()
+{
+    cout << "Wybirze opcjê\n";
+    int option;
+    cin >> option;
+    return option; 
+}
+
+void addNewBook(collectionOfBooks,currentNumberOfBooks)
+{
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int BOOK_SIZE = 100;
+    book collectionOfBooks[BOOK_SIZE];
+    int currentNumberOfBooks = 0;
+
+    while(true)
+    {
+        menu();
+        int choosenOption = getOption();
+
+        if (choosenOption == 0)
+            break;
+
+        switch (choosenOption)
+        {
+        case 1: 
+            addNewBook(collectionOfBooks, currentNumberOfBooks);
+            break;
+        }
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
