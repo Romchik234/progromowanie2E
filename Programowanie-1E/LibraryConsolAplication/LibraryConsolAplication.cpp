687b68448db8ce90737d374a29498cb058c62675
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-09 15:04:44 +0100

    09.01.2024

--- Programowanie-1E/LibraryConsolAplication/LibraryConsolAplication.cpp
+++ Programowanie-1E/LibraryConsolAplication/LibraryConsolAplication.cpp
@@ -14,6 +14,7 @@ void menu()
     system("cls");
     std::cout << "BIBLIOTEKA\n";
     std::cout << "1.Dodanoe nowej ksia¿ki \n";
+    std::cout << "2.Wyswietlenie wszystkich ksia¿ek \n";
 
     std::cout << "0.Koniec programu\n";
 
@@ -27,16 +28,42 @@ int getOption()
     return option; 
 }
 
-void addNewBook(collectionOfBooks,currentNumberOfBooks)
+void addNewBook(book books[], int &currentNumberOfBooks)
 {
+    currentNumberOfBooks++; 
+    cout << "Podaj tytu³:\n";
+    cin >> books[currentNumberOfBooks].title;
+    cout << "Podaj autora:\n";
+    cin >> books[currentNumberOfBooks].author;
+    cout << "Podaj rok publikacji:\n";
+    cin >> books[currentNumberOfBooks].publicationYear;
+    cout << "Podaj iloœæ stron:\n";
+    cin >> books[currentNumberOfBooks].numerOfPages;
+}
+
+void showAllBooks(book books[], int currentNumberOfBooks)
+{
+    system("cls");
+    cout << "Wyswietlanie wszystkich pozycji : \n";
     
+    for(int i = 0; i <= currentNumberOfBooks; i ++ )
+    {
+        cout << "---------------------------\n";
+        cout << "Tytu³: " << books[i].title << "\n";
+        cout << "Autor: " << books[i].author << "\n";
+        cout << "Rok wydania: " << books[i].publicationYear << "\n";
+        cout << "Iloœæ stron : " << books[i].numerOfPages << "\n";
+    }
+    system("pause");
 }
 
 int main()
 {
+    setlocale(LC_CTYPE, "polish");
+
     const int BOOK_SIZE = 100;
     book collectionOfBooks[BOOK_SIZE];
-    int currentNumberOfBooks = 0;
+    int currentNumberOfBooks = -1;
 
     while(true)
     {
@@ -44,13 +71,17 @@ int main()
         int choosenOption = getOption();
 
         if (choosenOption == 0)
-            break;
+            break; 
 
         switch (choosenOption)
         {
         case 1: 
             addNewBook(collectionOfBooks, currentNumberOfBooks);
             break;
+        case 2: 
+            showAllBooks(collectionOfBooks, currentNumberOfBooks); 
+            break;
         }
+        
     }
 }
