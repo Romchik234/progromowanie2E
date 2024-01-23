b140bb93b905adb5173fda377289bc5b304b5802
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-23 15:13:47 +0100

    23.01.2024 class

--- Programowanie-1E/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programowanie-1E/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 using namespace std;
 
-struct person
+class person				//klasa
 {
-	string name;
+public:
+	string name;			//pole
 	//string secondName;
 	string surname;
 	short age;
 	//short height;
-	//char sex; // M - male | F - female 
+	//char sex; // M - male | F - female
+	
+	void showInfo()			//metoda
+	{
+		cout << " Info o osoie : \n";
+		cout << "Imie : " << name << "\n";
+		cout << "Nazwisko : " << surname << "\n";
+		cout << "Wiek : " << age << "\n";
+
+
+	}
+
+	bool isLegalAge()
+	{
+		return age >= 18;
+	}
+
 };
 
 
 int main()
 {
-	person firstPerson;
+	person firstPerson;				//objekt
+
 	firstPerson.name = "Jan";
 	firstPerson.surname = "Kowalski";
 	firstPerson.age = 5;
+	firstPerson.showInfo();
 
-	cout << " Info o osoie : \n";
-	cout << "Imie : " << firstPerson.name << "\n";
-	cout << "Nazwisko : " << firstPerson.surname << "\n";
-	cout << "Wiek : " << firstPerson.age << "\n";
+	if (firstPerson.isLegalAge())
+		cout << "Pewnoletni\n";
+
+	person secondPerson;
+
+	secondPerson.name = "Bartek";
+	secondPerson.surname = "Nawak";
+	secondPerson.age = 56;
+	secondPerson.showInfo();
+ 
+	if(secondPerson.isLegalAge())
+		cout << "Pewnoletni\n";
+
+
 }
