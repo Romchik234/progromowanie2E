9b7e887568628c8f5e20931469c375d2413af050
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-30 14:54:37 +0100

    30.01.2024

--- Programowanie-1E/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programowanie-1E/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 class person				//klasa
 {
-public:
+private:
 	string name;			//pole
 	//string secondName;
 	string surname;
 	short age;
 	//short height;
 	//char sex; // M - male | F - female
-	
+public:
 	void showInfo()			//metoda
 	{
 		cout << " Info o osoie : \n";
@@ -26,6 +26,14 @@ public:
 		return age >= 18;
 	}
 
+	void SetAge(short a)
+	{
+		if (a > 0 && a < 130)
+			age = a;
+		else
+			cout << "B³êdna wartoœæ\n";
+
+	}
 };
 
 
@@ -33,9 +41,11 @@ int main()
 {
 	person firstPerson;				//objekt
 
-	firstPerson.name = "Jan";
-	firstPerson.surname = "Kowalski";
-	firstPerson.age = 5;
+	//firstPerson.name = "Jan";
+	//.surname = "Kowalski";
+	//firstPerson.age = 5;
+	firstPerson.SetAge(5000);
+
 	firstPerson.showInfo();
 
 	if (firstPerson.isLegalAge())
@@ -43,12 +53,12 @@ int main()
 
 	person secondPerson;
 
-	secondPerson.name = "Bartek";
-	secondPerson.surname = "Nawak";
-	secondPerson.age = 56;
+	//secondPerson.name = "Bartek";
+	//secondPerson.surname = "Nawak";
+	//secondPerson.age = 56;
 	secondPerson.showInfo();
- 
-	if(secondPerson.isLegalAge())
+
+	if (secondPerson.isLegalAge())
 		cout << "Pewnoletni\n";
 
 
