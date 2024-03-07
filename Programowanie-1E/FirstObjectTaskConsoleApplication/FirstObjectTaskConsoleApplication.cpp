a5e275df26cd84a9f08af962dfd16270ab091f49
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-07 15:09:15 +0100

    07.03.2024

--- Programowanie-1E/FirstObjectTaskConsoleApplication/FirstObjectTaskConsoleApplication.cpp
+++ Programowanie-1E/FirstObjectTaskConsoleApplication/FirstObjectTaskConsoleApplication.cpp
@@ -2,6 +2,28 @@
 #include <iostream>
 using namespace std;
 
+/*
+Zdefiniuj klase opisuj¹cy punkt na przestrzeni kartezjañskiej. Mo¿liwoœci klasy:
+* konstruktory
+* metoda która zwórci odleg³oœæ tego punktu od poczatku uk³adu wspó³rzednych
+* metoda która zwróci odleg³oœæ od innego punktu
+* metoda która zwróci informacjê w której æwiartce uk³adu wspó³rzednych siê znajduje punkt
+* metodê która poka¿e informacje o punkcie.
+
+Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
+
+*/
+
+/*
+Zdefiniuj klase opisuj¹ce konto bankowe. Mo¿liwoœci klasy:
+* konstruktory
+* metoda która dokona wp³aty na konto
+* metoda która wyp³aty z konta
+* metoda która wykona przelew na inne konto
+* metodê która poka¿e informacje o koncie.
+
+Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
+*/     
 
 class point
 {
@@ -19,93 +41,79 @@ public:
 		quarter = 0;
 	}
 
-	void showInfo()
+	point(string name, float x, float y, short quarter)
 	{
-		cout << "Name of point:  " << name; 
-		cout << "wspo³rzêdne x= " << xLine << "  y= " << yLine; 
-		quarter = getQuarter(xLine,yLine);
+		name = name; 
+		xLine = x;
+		yLine = y; 
+		quarter = quarter; 
+   	}
+
+	void declarationPoint()
+	{
+		cout << "Podaj nazwe punktu: \n";
+		cin >> name;
+		cout << "Podaj wspolrzedne x: \n";
+		cin >> xLine;
+		cout << "Podaj wspolrzedne y: \n";
+		cin >> yLine;
+		quarter = findQuarter(xLine, yLine);
 	}
 
-	short getQuarter(float x,float  y)
+	short findQuarter(float xLine, float yLine)
 	{
-		if (x > 0 && y > 0)
+		if (xLine > 0 && yLine > 0)
 			return 1;
-		if (x > 0 && y < 0)
+		if (xLine > 0 && yLine < 0)
 			return 4;
-		if (x < 0 && y < 0)
+		if (xLine < 0 && yLine > 0)
+			return 2;
+		if (xLine < 0 && yLine < 0)
 			return 3;
 		else
-			return 2;
+			return 0;
+	}
+
+	void showInfoofPoint()
+	{
+		cout << "\n----------------------------\n";
+		cout << "imie punktu: " << name << " \n";
+		cout << "wspo³rzednde x: " << xLine << " \n";
+		cout << "wspo³rzednde y: " << yLine << " \n";
+		cout << "czwartka : " << quarter << " \n";
+		cout << "odleglosc od poczatku: " << sqrt(xLine * xLine + yLine * yLine) << " \n\n";
+
+	}
+
+	void distanceBetweenPoints(point firstPoint)
+	{
+		cout << "Odleglosc pomiedzy punktami :  " << sqrt((xLine - firstPoint.xLine) * (xLine - firstPoint.xLine) + (yLine - firstPoint.yLine) * (yLine - firstPoint.yLine)) << " \n";
 	}
 };
-
-void menu()
+void task1()
 {
-	system("cls");
-	std::cout << "Uk³ad Wspó³rzêdnych \n";
-	cout << "1.Informacja o punktach\n";
-	cout << "2.Nowy punkt\n";
-	cout << "3.liczenie pomiêdzy punktów\n";
-	cout << "0.Koniec programu\n";
+	point firstPoint;
+	point secondPoint;
+	point thirdPoint("Opa", 3, 4, 1);
 
+	firstPoint.declarationPoint();
+	firstPoint.showInfoofPoint();
+
+
+
+	secondPoint.declarationPoint();
+	secondPoint.showInfoofPoint();
+	secondPoint.distanceBetweenPoints(firstPoint);
+
+	cout << "\n" << "----------------\n";
+	thirdPoint.showInfoofPoint();
 }
-
-int getOption()
-{
-	cout << " Wybierz opcjê: \n";
-	int option;
-	cin >> option;
-	return option;
-}
-
-
-
-void makeNewPunkt()
-{
-
-
-}
-
-void distanceCounting()
-{
-
-
-
-}
-
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
-
-	point firstPoint;
-	while (true)
-	{
-		menu();
-		int schoosenOption = getOption();
-
-		if (schoosenOption == 0)
-			break;
-
-		switch (schoosenOption)
-		{
-		case 1:
-			firstPoint.showInfo();
-			break;
-		case 2: 
-			makeNewPunkt();
-			break;
-		case 3:
-			distanceCounting(); 
-			break;
-		}
-
-	}
-
-
-
+	task1();
+	
 
 
 }
-
-
