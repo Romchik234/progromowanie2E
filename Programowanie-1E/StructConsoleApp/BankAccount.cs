573720a7e499297e0f45048b7b788bf4b6b38afa
q <q>
2024-10-01 08:09:31 +0200

    01.10.2024 structur csharp

---
+++ Programowanie-1E/StructConsoleApp/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructConsoleApp
+{
+    struct BankAccount
+    {
+        double balance;
+        string PIN;
+        string owner;
+        string currency;
+    }
+}
