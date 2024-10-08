bdd03fc32d61918f281eac8de843217c236468f7
q <q>
2024-10-08 08:07:44 +0200

    08.10.2024

---
+++ Programowanie-1E/FirstObjectConsoleApp/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstObjectConsoleApp
+{
+    internal class BankAccount
+    {
+            public double balance;
+            public string PIN;
+            public string owner;
+            public string currency;
+    }
+}
