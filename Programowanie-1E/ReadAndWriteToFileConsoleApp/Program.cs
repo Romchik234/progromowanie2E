3526d53e1077dee5fc6491ac14dc47f5af3be485
q <q>
2024-10-15 08:08:42 +0200

    15.10.2024 Odczyt z pliku

--- Programowanie-1E/ReadAndWriteToFileConsoleApp/Program.cs
+++ Programowanie-1E/ReadAndWriteToFileConsoleApp/Program.cs
@@ -12,7 +12,19 @@ age = int.Parse(Console.ReadLine());
 StreamWriter streamWriter = new StreamWriter("C:\\Romchik Khazov\\progromowanie2E\\Programowanie-1E\\OpachaPlik.txt", true);
 
 streamWriter.WriteLine(name);
-streamWriter.Flush(); 
+streamWriter.Flush();
 streamWriter.WriteLine(age);
 
 streamWriter.Close();
+
+StreamReader streamReader = new StreamReader("C:\\Romchik Khazov\\progromowanie2E\\Programowanie-1E\\OpachaPlik.txt");
+while (true)
+{
+    name = streamReader.ReadLine();
+    if (name == null)
+        break;
+    age = int.Parse(streamReader.ReadLine());
+    Console.WriteLine($"Odczytano z pliku: {name} , {age}"); 
+}
+
+streamReader.Close();
