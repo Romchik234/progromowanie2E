6ae43131dff49afe7daaef77fb4c7159fd326550
q <q>
2024-11-22 12:29:09 +0100

    22.11.2024

--- Programowanie-1E/LinqConsoleApp/Program.cs
+++ Programowanie-1E/LinqConsoleApp/Program.cs
@@ -25,6 +25,13 @@ person.age = 30;
 
 people.Add(person);
 
+person = new Person();
+person.name = "Ewa";
+person.surname = "Kowalczyk";
+person.age = 46;
+
+people.Add(person);
+
 #endregion
 
 Console.WriteLine("Wszystkie osoby: ");
@@ -42,3 +49,52 @@ from people
 int maxAge = people.Max(p => p.age);
 Console.WriteLine($"Najstarsza osoba ma {maxAge} lat ");
 Console.WriteLine(); 
+
+double avgAge = people.Average(p => p.age);
+Console.WriteLine($"sredni wiek osób to {avgAge}");
+Console.WriteLine();
+
+if (people.Any(p => p.age < 18))
+    Console.WriteLine("Jest osoba nie pełnio letnia");
+
+if (people.All(p => p.age >= 18))
+    Console.WriteLine("Wszystkie ososby są pełnioletni");
+
+List<Person> legalAgePeople = people.Where(p => p.age >= 18).ToList();
+Console.WriteLine("Lista osób pełnie letnich : ");
+foreach ( Person p in legalAgePeople)
+{
+    Console.WriteLine($"Imie: {p.name} Nazwisko: {p.surname} wiek:{p.age} ");
+}
+Console.WriteLine();
+
+List<Person> sortByName = people.OrderBy(p => p.surname).ToList();
+Console.WriteLine("Posortowano po Nazwisku : ");
+foreach (Person p in sortByName)
+{
+    Console.WriteLine($"Imie: {p.name} Nazwisko: {p.surname} wiek:{p.age} ");
+}
+Console.WriteLine();
+
+var sortByNameandByAge = people.OrderBy(p => p.surname).ThenBy(p => p.age);
+Console.WriteLine("Posortowano po Nazwisku I WIEKU  : ");
+foreach (Person p in sortByNameandByAge)
+{
+    Console.WriteLine($"Imie: {p.name} Nazwisko: {p.surname} wiek:{p.age} ");
+}
+Console.WriteLine();
+
+var groupByName = people.GroupBy(p => p.name);
+Console.WriteLine("Po grupowao po imieniu: ");
+foreach (var group in groupByName)
+{
+    Console.WriteLine( group.Key);
+
+    Console.WriteLine("Lista osób w grupie: ");
+    //var x = people.Where(p => p.name == group.Key);
+    foreach (Person p in group)
+    { 
+        Console.WriteLine($"Imie: {p.name} Nazwisko: {p.surname} wiek:{p.age} ");
+    }
+}
+Console.WriteLine();
