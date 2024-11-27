8d72790b0ea9238e2fc1bcc6b5ed30a2d4454f72
q <q>
2024-11-27 14:20:25 +0100

    linq tasks 27.11.2024

---
+++ Programowanie-1E/LinqTasksConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿//LinqTasksConsoleApp
+
+using LinqTasksConsoleApp;
+
+var tasks = new List<TaskItem>
+{
+    new TaskItem(1, "Buy groceries", true),
+    new TaskItem(2, "Clean the house", false),
+    new TaskItem(3, "Pay bills", true),
+    new TaskItem(4, "Study LINQ", false),
+    new TaskItem(5, "Exercise", true)
+};
+
+Console.WriteLine("Wszystkie elementy kolekcji: ");
+foreach (var task in tasks)
+{
+    Console.WriteLine(task);
+}
+// Zadanie 1: Wyszukaj wszystkie zakończone zadania
+Console.WriteLine("Zadanie1");
+var isCompleted = tasks.Where(t => t.IsCompleted == true);
+foreach (var task in isCompleted)
+{
+    Console.WriteLine(task);
+}
+
+// Zadanie 2: Znajdź pierwsze zadanie, które nie jest zakończone
+Console.WriteLine("Zadanie 2");
+var isntCompleted = tasks.First(t => t.IsCompleted != true);
+Console.WriteLine(isntCompleted); 
+
+// Zadanie 3: Posortuj zadania według nazwy
+
+// Zadanie 4: Policz zadania zakończone
+
+// Zadanie 5: Wybierz tylko nazwy zadań i wyświetl
+
+// Zadanie 6: Znalezienie nazw zakończonych zadań posortowanych według długości nazwy
+
+//Zadanie 7: Zadania pogrupowane według stanu zakończenia, a następnie posortowane w grupach według nazwy
+
+//Zadanie 8: Najkrótsza nazwa zadania niezakończonego
+
+//Zadanie 9: Ilość liter w nazwach wszystkich zakończonych zadań
+
+//Zadanie 10: Lista zadań z indeksami (zakończone zadania z numeracją)
+
+//Zadanie 11: Zadania z najdłuższą nazwą w każdej grupie zakończonych i niezakończonych
+
+//Zadanie 12: Zlicz, ile zadań w każdej grupie zakończonych i niezakończonych zawiera słowo „the” w nazwie
+
+//Zadanie 13: Utwórz listę zakończonych zadań z ich numeracją oraz długością nazw
+
+//Zadanie 14: Zadania posortowane według stanu zakończenia, a następnie alfabetycznie według nazw
+
+//Zadanie 15: Sprawdź, czy w nazwach wszystkich zadań są co najmniej 2 różne samogłoski
+
+//Zadanie 16: Znajdź wszystkie unikalne litery używane w nazwach zadań zakończonych
