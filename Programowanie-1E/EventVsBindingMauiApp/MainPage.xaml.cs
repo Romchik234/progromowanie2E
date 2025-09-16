de41e90ad719d4df13c7a17e36e63bb0400509ea
Sala213 <Sala213@ezse.pl>
2025-09-16 12:30:24 +0200

    16.09.2025 bindings

---
+++ Programowanie-1E/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace EventVsBindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Message { get; set; }
+
+        private string returnMessage;
+
+        public string ReturnMessage
+        {
+            get { return returnMessage; }
+            set
+            {
+                returnMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void sliderFrom_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            labelRotation.Rotation = sliderFrom.Value;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ReturnMessage = "Ilość znaków " + Message.Length;
+        }
+    }
+}
---
+++ Programowanie-1E/SimpleCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(firstNumberEntry.Text, out int firstNumber)
+                || int.TryParse(secondNumberEntry.Text, out int secondNumber))
+                return;
+
+            int result = firstNumber + secondNumber;
+
+            resultLabel.Text = "Wynik to : " + result.ToString();
+        }
+    }
+}
