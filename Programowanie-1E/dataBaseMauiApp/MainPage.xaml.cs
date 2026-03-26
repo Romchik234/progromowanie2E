923e069cca9a48fec74f5ed447b48eb240f516f1
Uczen <Uczen@LAPTOP-26CG11IL>
2026-03-26 09:55:06 +0100

    new laptop 26.06.2026

--- Programowanie-1E/dataBaseMauiApp/MainPage.xaml.cs
+++ Programowanie-1E/dataBaseMauiApp/MainPage.xaml.cs
@@ -1,8 +1,20 @@
-﻿namespace dataBaseMauiApp
+﻿using PeopleRepositoryClassLibrary;
+using PeopleRepositoryClassLibrary.Models;
+
+namespace dataBaseMauiApp
 {
+
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        PeopleRepository peopleRepository = new PeopleRepository();
+        private List<Person> people;
+
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = peopleRepository.GetAllPeople(); }
+        }
+
 
         public MainPage()
         {
@@ -11,17 +23,10 @@
 
         private void OnCounterClicked(object? sender, EventArgs e)
         {
+            List<Person> list = new List<Person>();
+            
 
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
-
-
+        
     }
 }
