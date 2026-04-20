05106dc9c2507c3754c79ffb76d309b1422b401a
Uczen <Uczen@LAPTOP-26CG11IL>
2026-04-20 08:18:40 +0200

    20.04.2026

---
+++ Programowanie-1E/QuizDB/QuizRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizDB;
+using QuizDB.Models;
+
+namespace QuizDB
+{
+    public class QuizRepository
+    {
+
+        private QuizDB context; 
+
+        public QuizRepository()
+        {
+            context = new QuizDB();
+        }
+        
+        public List<Question> GetQuestion()
+        {
+            return context.Questions
+                .AsNoTracking()
+                .Include(p => p.Answers)
+                .OrderBy(p => p.Id)
+                .ToList();
+        }
+
+    }
+
+}
---
+++ Programowanie-1E/QuizMauiApp/MainVievModelQuiz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizDB;
+using QuizDB.Models;
+
+namespace QuizMauiApp
+{
+    internal class MainVievModelQuiz : BindableObject
+    {
+        public ObservableCollection<Question> Questions { get; set; }
+
+       /* private Command? numberOfQ = null;
+        
+        public Command NumberOfQ
+        {
+            get {if(numberOfQ == null)
+                    numberOfQ = new Command(
+                        () =>
+                        {
+                            Questions.Clear();
+                            for
+                        }
+                        )
+        }
+
+        */
+
+
+
+
+
+    }
+}
